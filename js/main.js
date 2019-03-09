f5916c85e803794817f834713782e37935c50e1a
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-03-09 14:19:30 -0300

    layout tweaks to .overlay and it's contents; add programming language stack icons to top right with tooltips; remove old useless images from images folder; add favicon; add arrow button to toggle project overlays

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(".projeto").click(function() {
-  $(this).children(".overlay").fadeToggle('fast');
+$(".arrow-down").click(function() {
+  $(this).next(".overlay").slideToggle('fast');
+
+  if ($(this).data('rotated') == true) {
+    $(this).css('transform', 'rotate(0deg)');
+    $(this).data('rotated', false);
+  } else {
+    $(this).css('transform', 'rotate(180deg)');
+    $(this).data('rotated', true);
+  }
 })
